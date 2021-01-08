6947f60f774b1a0f481b17be6bd6716460d1da9c
Leandro Solis <leandrosolismora@estudiantec.cr>
2021-01-08 13:24:31 -0600

    se implementa el algoritmo genetico

--- algoritmo_genetico.py
+++ algoritmo_genetico.py
@@ -1,9 +1,11 @@
+import math
+import random
 
 def optimizar(dominio, tam_pobl, porc_elite, prob_mut, reps):
     """Algoritmo genético para optimización estocástica.
 
     Entradas:
-    dominio (DominioAG)
+    dominio ( instancia dominio_ag_tsp)
         Un objeto que modela el dominio del problema que se quiere aproximar.
     
     tam_pobl (int)
@@ -23,5 +25,24 @@ def optimizar(dominio, tam_pobl, porc_elite, prob_mut, reps):
         aproximación a la mejor solución al problema.
     """
 
-    # Pendiente: implementar este método
-    pass
+    # Pendiente: implementar este método    FALTA LO DE LA APTITUD
+    poblacion = dominio.generar_n(tam_pobl)
+    while reps > 0: 
+        for solucion in poblacion: 
+            solucion.aptitud = dominio.fcosto(solucion)
+        orderarAptitud(poblacion)
+        num_padres = math.floor(len(poblacion)*porc_elite)
+        num_hijos = len(poblacion) - num_padres
+        sig_geneneracion = poblacion[0:num_padres]
+        descendencia=[]
+        while num_hijos > 0:
+            padre_A = sig_geneneracion[random.randint(0,len(sig_geneneracion)-1)]
+            padre_B = sig_geneneracion[random.randint(0,len(sig_geneneracion)-1)]
+            hijo = dominio.cuzar(padre_A,padre_B)
+            if (random.randint(0,100) < prob_mut*100):
+                hijo = dominio.mutar(hijo)
+            descendencia.append(hijo)
+            num_hijos -= 1
+        sig_geneneracion.append(descendencia)
+        poblacion=sig_geneneracion
+        reps -= 1
